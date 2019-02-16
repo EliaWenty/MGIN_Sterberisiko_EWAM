7671277835333d93a00621f0e7617e1b6ce70767
eliwe <eliwen@gmx.at>
2019-02-16 12:19:30 +0100

    es steht in den TODO was zu machen ist

---
+++ src/GUI.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+import java.time.format.TextStyle;
+
+public class GUI extends Application{
+
+    GridPane window = new GridPane();
+    TextField eingabeHoehe = new TextField();
+    TextField eingabeDruckStaerke = new TextField();
+    TextField eingabealter = new TextField();
+    Label ergebniss = new Label();
+    Logik logic = new Logik();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button berechnen = new Button("berechnen");
+
+        berechnen.setOnAction(new EventHandler<ActionEvent>(){
+           @Override
+           public void handle(ActionEvent event){
+               //TODO
+               //Methode aufrufen ihr die werte der Textfielder zuweisen
+               //und das ergebniss dem ergebniss zuweisen
+               //Ich füg dann noch paar verschönerungssachen ein
+
+           }
+        }
+        );
+
+        window.add(eingabeHoehe, 0,0);
+        window.add(eingabeDruckStaerke, 0,1);
+        window.add(eingabealter, 0,2);
+        window.add(berechnen, 0,3);
+        window.add(ergebniss, 0,4);
+
+
+        primaryStage.setTitle("Sterberisiko");
+        primaryStage.setScene(new Scene(window, 200, 200));
+        primaryStage.show();
+    }
+}
---
+++ src/Logik.java
@@ -0,0 +1,17 @@
+public class Logik {
+
+
+    //TODO
+    //entweder aus Lukas Programm kopieren oder eine schönere Lösung finden
+    //ich würds mit einer schöneren machen
+    public String SterberisikoMethode(Integer hoehe, Integer druckSStaerke, Integer alter){
+        if(alter >= 17 && alter <= 19){
+            System.out.println("alter zwischen 17 und 19");
+
+        }
+        else if (alter >= 20 && alter <= 24){
+            System.out.println("alter zwischen 20 und 24");
+        }
+        return null ;
+    }
+}
