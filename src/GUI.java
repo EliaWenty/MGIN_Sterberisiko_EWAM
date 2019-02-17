3993fc52337919620af23ca5b31ebf18efd30cb4
eliwe <eliwen@gmx.at>
2019-02-17 14:08:38 +0100

    es steht in den TODO was zu machen ist

--- src/GUI.java
+++ src/GUI.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 
+
 import java.time.format.TextStyle;
 
 public class GUI extends Application{
@@ -28,20 +29,38 @@ public class GUI extends Application{
                //Methode aufrufen ihr die werte der Textfielder zuweisen
                //und das ergebnis dem ergebnis zuweisen
                //Ich füg dann noch paar verschönerungssachen ein
-
+               try {
+                   ergebnis.setText(logic.SterberisikoMethode(
+                           Integer.parseInt(eingabeHoehe.getText()),
+                           Float.valueOf(eingabeDruckStaerke.getText()),
+                           Integer.parseInt(eingabealter.getText())));
+               }catch (NullPointerException e){
+                   System.out.println(e);
+                   ergebnis.setText("bitte Werte eingeben");
+               }
            }
         }
         );
 
-        window.add(eingabeHoehe, 0,0);
-        window.add(eingabeDruckStaerke, 0,1);
-        window.add(eingabealter, 0,2);
+        Label kommentarHoehe = new Label("Hoehe (zw. 150cm und 184cm):");
+        Label kommentarDruckStaerke = new Label("DruckStaerke (zw. 27.8 und 35.0):");
+        Label kommentarAlter = new Label("Alter (zw. 17 und 24):");
+
+
+        window.add(eingabeHoehe, 1,0);
+        window.add(eingabeDruckStaerke, 1,1);
+        window.add(eingabealter, 1,2);
+
+        window.add(kommentarHoehe, 0,0);
+        window.add(kommentarDruckStaerke, 0,1);
+        window.add(kommentarAlter, 0,2);
+
         window.add(berechnen, 0,3);
         window.add(ergebnis, 0,4);
 
 
         primaryStage.setTitle("Sterberisiko");
-        primaryStage.setScene(new Scene(window, 200, 200));
+        primaryStage.setScene(new Scene(window, 330, 130));
         primaryStage.show();
     }
 }
